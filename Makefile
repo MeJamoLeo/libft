df0552d6e1a343196d47d4bde8275a8c33d0a5c5
mejamoleo <reonarudo11919273@gmail.com>
2021-04-15 15:20:17 +0900

    (done) ft_memcmp
    norm && unit_test

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ SRCS	= ft_toupper.c \
 			ft_memcpy.c \
 			ft_memccpy.c \
 			ft_memmove.c \
-			ft_memchr.c
+			ft_memchr.c \
+			ft_memcmp.c
 
 OBJS	= $(SRCS:%.c=%.o)
 CC		= gcc
---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 15:08:45 by treo              #+#    #+#             */
+/*   Updated: 2021/04/15 15:17:50 by treo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr_s1;
+	const unsigned char	*ptr_s2;
+	size_t				i;
+
+	ptr_s1 = (const unsigned char *)s1;
+	ptr_s2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (*(ptr_s1 + i) != *(ptr_s2 + i))
+			return (*(ptr_s1 + i) - *(ptr_s2 + i));
+		i++;
+	}
+	return (0);
+}
