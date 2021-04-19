35e995543b8cc8f647d48d6ffc304a1829e3f8ac
mejamoleo <reonarudo11919273@gmail.com>
2021-04-20 00:20:09 +0900

    (fix) ft_strdup
    remove garbage line

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 15:27:19 by treo              #+#    #+#             */
-/*   Updated: 2021/04/19 16:14:03 by treo             ###   ########.fr       */
+/*   Updated: 2021/04/20 00:16:15 by treo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ char	*ft_strdup(const char *s1)
 	char	*ptr;
 
 	len = ft_strlen(s1);
-	ptr = (char *)ft_calloc(len, sizeof(char));
 	ptr = (char *)malloc(len * sizeof(char) + 1);
 	if (!ptr)
 		return (NULL);
