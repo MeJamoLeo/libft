e49e1351ad576ee5d9f14bba64bcdff1f28e83bf
mejamoleo <reonarudo11919273@gmail.com>
2021-04-20 10:17:19 +0900

    (done) ft_strjoin
    fix norm && unit-test

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 01:54:48 by treo              #+#    #+#             */
-/*   Updated: 2021/04/20 09:47:00 by treo             ###   ########.fr       */
+/*   Updated: 2021/04/20 10:16:54 by treo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	unsigned int	len_s1;
-	unsigned int	len_s2;
+	unsigned int	len;
 	char			*ptr;
+	unsigned int	i;
+	unsigned int	j;
 
 	if (!(s1 && s2))
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	ptr = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc((len + 1) * sizeof(char));
 	if (!ptr)
 		return (NULL);
-	ft_strlcat(ptr, s1, len_s1 + 1);
-	ft_strlcat(ptr, s2, len_s1 + len_s2 + 1);
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		*(ptr + i) = *(s1 + i);
+		i++;
+	}
+	j = 0;
+	while (*(s2 + j) != '\0')
+	{
+		*(ptr + i + j) = *(s2 + j);
+		j++;
+	}
+	*(ptr + len) = '\0';
 	return (ptr);
 }
