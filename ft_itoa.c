a3be3f25121dc1c297e36001284ca89c7ec2d1c5
mejamoleo <reonarudo11919273@gmail.com>
2021-04-22 18:23:27 +0900

    (done)

---
+++ ft_itoa.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/22 14:47:22 by treo              #+#    #+#             */
+/*   Updated: 2021/04/22 18:22:17 by treo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*fill_string(char *dst, int n, unsigned int s_len)
+{
+	int	tmp;
+
+	tmp = n;
+	if (n < 0)
+		n *= -1;
+	while (n >= 10)
+	{
+		*(dst + (s_len--) - 1) = (n % 10) + '0';
+		n /= 10;
+	}
+	*(dst + (s_len--) - 1) = n + '0';
+	if (n < 0)
+		*(dst) = '-';
+	*(dst + ft_strlen(dst)) = '\0';
+	return (dst);
+
+}
+
+static int	digit_count(unsigned int n)
+{
+	int	i;
+	while (n >= 10)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	int	s_len;
+	int	tmp;
+	char *ptr;
+
+	s_len = 0;
+	tmp = n;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (tmp < 0)
+	{
+		tmp *= - 1;
+		s_len += 1;
+	}
+	s_len += digit_count(tmp);
+	ptr = ft_calloc(s_len + 1, sizeof(char));
+	if (!ptr)
+		return (NULL);
+	fill_string(ptr, n, s_len);
+	return (ptr);
+}
